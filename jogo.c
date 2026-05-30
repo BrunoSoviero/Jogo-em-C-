89cf70593ee0f4131e188f55e1f5cf4fa2df6b1a
BrunoSoviero <sovierobruno@gmail.com>
2026-05-30 12:33:14 -0300

    Adição dos mapas 2 e 3 e movimentação que identifica a porta

--- jogo.c
+++ jogo.c
@@ -24,6 +24,8 @@ int main(){
     Monstro monstros[10];
     int numMonstros = 0;
    
+    int fimDeJogo = 0;
+    int faseAtual = 1;
     
 
     retornoMenu = Menu();
@@ -32,7 +34,7 @@ int main(){
     case 0:
         carregaMapa("mapas/Mapa1.txt", m, &bombeiro);
         
-         float blocoTamanhoH = (float)GetScreenHeight() / MAPA_ALTURA;
+        float blocoTamanhoH = (float)GetScreenHeight() / MAPA_ALTURA;
         float blocoTamanhoV = (float)GetScreenWidth()  / MAPA_LARGURA;
         float blocoTamanho  = (blocoTamanhoH < blocoTamanhoV) ? blocoTamanhoH : blocoTamanhoV;
         initMonstro(monstros, &numMonstros, m, blocoTamanho);
@@ -40,15 +42,47 @@ int main(){
         while(!IsKeyPressed(KEY_ESCAPE) && retornoJogo == 0){
         retornoJogo = movimentoPersonagem(m, &bombeiro);
 
-         desenhaMapa(texturas, m, &bombeiro);
-        for(int k = 0; k < numMonstros; k++){
-            atualizaMonstro(&monstros[k], m, texturas, &bombeiro);
+         if (retornoJogo == 1)
+        {
+            if (faseAtual == 1)
+            {
+                // Avança para o Mapa 2
+                carregaMapa("mapas/Mapa2.txt", m, &bombeiro);
+                initMonstro(monstros, &numMonstros, m, blocoTamanho);
+                faseAtual = 2; 
+                retornoJogo = 0; // Reseta para continuar jogando no mesmo loop
+            }
+            else if (faseAtual == 2)
+            {
+                carregaMapa("mapas/Mapa3.txt", m, &bombeiro);
+                initMonstro(monstros, &numMonstros, m, blocoTamanho);
+                faseAtual = 3; 
+                retornoJogo = 0; // Reseta novamente para continuar no loop
+            }
+            else if (faseAtual == 3)
+            {
+                // Se venceu o mapa 3 (que agora é o último), vitória total!
+                printf("Parabens! Voce fechou o jogo inteiro!\n");
+                fimDeJogo = 1;
+            }
+        }
+            // Se o jogador perdeu
+            else if (retornoJogo == 2)
+            {
+                printf("Voce Perdeu!\n");
+                fimDeJogo = 1; // Encerra o loop do jogo
+            }
+
+        if (!fimDeJogo)
+        {
+            desenhaMapa(texturas, m, &bombeiro);
+            for(int k = 0; k < numMonstros; k++)
+            {
+                atualizaMonstro(&monstros[k], m, texturas, &bombeiro);
             } 
         }
-        
-        if(retornoJogo == 2){
-            printf("Voce Perdeu!");
-        }
+    }
+    break;
         break;
     case 2:
         CloseWindow();
@@ -56,7 +90,6 @@ int main(){
     default:
         break;
     }
-    
     UnloadTexture(texturas.escada);
     UnloadTexture(texturas.plataforma);
     UnloadTexture(texturas.porta);
